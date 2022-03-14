a12a5e7ed32e7a015e2423bbc34499539ecf07be
OnJourney <oj@onjourney.co.kr>
2022-03-14 17:33:21 +0900

    업데이트 내역 구현

--- public/electron.js
+++ public/electron.js
@@ -2,8 +2,35 @@ const { app, BrowserWindow } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const path = require("path");
 const isDev = require("electron-is-dev");
+const log = require("electron-log");
+const Store = require("electron-store");
 
+const store = new Store();
+const version = app.getVersion();
 let win;
+
+const createReleaseNoteWindow = () => {
+  let releaseNoteWindow = new BrowserWindow({
+    width: 600,
+    height: 800,
+    resizable: false,
+    autoHideMenuBar: true,
+  });
+
+  releaseNoteWindow.on("closed", () => {
+    releaseNoteWindow = null;
+  });
+
+  releaseNoteWindow.loadURL(
+    isDev ? "http://localhost:3000/releaseNote.html" : `file://${path.join(__dirname, "../build/releaseNote.html")}`
+  );
+
+  if (isDev) {
+    releaseNoteWindow.webContents.openDevTools({ mode: "detach" });
+  }
+
+  return releaseNoteWindow;
+};
 
 const createDefaultWindow = () => {
   win = new BrowserWindow();
@@ -22,7 +49,17 @@ const createDefaultWindow = () => {
 };
 
 app.on("ready", () => {
-  autoUpdater.checkForUpdatesAndNotify();
+  const storedVersion = store.get("version");
+  autoUpdater.checkForUpdates();
+
+  if (storedVersion) {
+    if (storedVersion !== version) {
+      createReleaseNoteWindow();
+      store.set("version", version);
+    }
+  } else {
+    store.set("version", version);
+  }
 });
 
 app.on("window-all-closed", () => {
@@ -30,23 +67,30 @@ app.on("window-all-closed", () => {
 });
 
 autoUpdater.on("checking-for-update", () => {
-  console.info("Checking for update...");
+  log.info("업데이트 체크중...");
 });
 
 autoUpdater.on("update-available", (info) => {
-  console.info("Update available.");
+  log.info("업데이트 가능");
 });
 
 autoUpdater.on("update-not-available", (info) => {
-  console.info("Update not available.");
+  log.info("최신버전 입니다.");
   createDefaultWindow();
 });
 
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - 현재 " + progressObj.percent + "%";
+  log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
+  log.info(log_message);
+});
+
 autoUpdater.on("error", (err) => {
-  console.info("Error in auto-updater. " + err);
+  log.info("에러발생: " + err);
 });
 
 autoUpdater.on("update-downloaded", (info) => {
-  console.info("Update downloaded");
+  log.info("업데이트 완료");
   autoUpdater.quitAndInstall();
 });
